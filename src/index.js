5c9da9d0d7ce6bde454dccabb48427c7fa6dd1f6
Graham Cox <graham@grahamcox.co.uk>
2017-06-11 08:40:09 +0100

    Handlers for a Secure and an Insecure response

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const config = require('./config');
 const app = express();
 app.use(express.static('src/public'));
 
+app.get('/api/insecure', (req, res) => {
+    res.send('Insecure response');
+});
+
+app.get('/api/secure', (req, res) => {
+    res.send('Secure response');
+});
+
 const port = config.get('http.port');
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
