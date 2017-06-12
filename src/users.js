71416ee3bd8b1f82bb2b119edd654ac1b57b4e41
Graham Cox <graham@grahamcox.co.uk>
2017-06-12 19:42:41 +0100

    User datastore

---
+++ src/users.js
@@ -0,0 +1,36 @@
+// The actual database of users
+const users = [];
+
+// Get a single user by their Internal ID
+function getUserById(id) {
+    return users.find((u) =>
+        u.id == id);
+}
+
+// Get a single user by their External ID with a specified provider
+function getUserByExternalId(provider, id) {
+    return users.find((u) =>
+        u.providers.findIndex((p) => p.provider == provider && p.id == id) >= 0);
+}
+
+// Create a new user
+function createUser(name, provider, id) {
+    const user = {
+        id: users.length,
+        name: name,
+        providers: [
+            {
+                provider: provider,
+                id: id
+            }
+        ]
+    };
+    users.push(user);
+    return user;
+}
+
+module.exports = {
+    getUserById: getUserById,
+    getUserByExternalId: getUserByExternalId,
+    createUser: createUser
+};
